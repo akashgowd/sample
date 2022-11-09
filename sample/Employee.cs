968fde3410d328aa33afca922e51f2af4cfd5948
derajea <derajea@DERAKA-39-05-22>
2022-11-09 11:16:51 +0530

    inserted

---
+++ sample/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    internal class Employee
+    {
+        public void employee()
+        {
+            int id = 1;
+            return;
+
+        }
+        public void list()
+        {
+            return;
+
+        }
+    }
+}
